9f8c8d99abe7a5abc93ba89fcb740275ef020797
Jonathan Penry <jonathan.penry@gmail.com>
2021-02-19 14:09:10 -0600

    fixes errors in MealPlannerReducer hyphens should have been underscores

--- src/Redux/Reducers/MealPlannerReducer.js
+++ src/Redux/Reducers/MealPlannerReducer.js
@@ -3,9 +3,9 @@ import DELETE_MEAL from "../Actions";
 
 function mealPlannerReducer(state = [], action){
     switch (action.type){
-        case ADD-MEAL:
+        case ADD_MEAL:
             return [...meal, action.meal];
-        case DELETE-MEAL:
+        case DELETE_MEAL:
             let newState = [...meal];
             let idx = newState.findIndex((meal) => meal.id === action.id) //"meal" could be "val" or anything else. it's like [i] in a for loop 
             newState.splice(idx, 1);                //splice does not make a new copy. delete starting at index(idx) deteremined from using find on the prior line and how many you want to delete (1)
