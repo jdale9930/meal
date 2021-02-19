43903a6bfc7091ca43fca5702c64553cdc126fd5
Jonathan Penry <jonathan.penry@gmail.com>
2021-02-19 13:49:11 -0600

    updates userReducer file

--- src/Redux/Reducers/UserReducer.js
+++ src/Redux/Reducers/UserReducer.js
@@ -1,2 +1,17 @@
-// Whatever you told the actions to trigger needs to have instructions in reducer
+//Whatever you define in UserActions needs to be told what to do in UserReducer
+import {SET_USER, CLEAR_USER} from "../actions";
 
+const initialUserState = {userName: "", id: null, role: ""};
+
+function userReducer(state = initialUserState, action){
+    switch (action.type) {
+        case SET_USER:
+           return {...state, username: action.username}; 
+        case CLEAR_USER:
+            return {...state, username: ""};
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
