49b767df726dfa3d829bcc5f64e38e28f3b334cc
Joshua Dale <jdale9930@gmail.com>
2021-02-19 13:27:30 -0600

    started routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SearchPage from "./components/Search"
+import {Switch, Route, BrowserRouter, Redirect, NavLink} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route>
+        
+      </Route>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
