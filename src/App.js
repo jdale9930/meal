c8a88c67429e63b17a8bef4f2acd9536807608d2
Joshua Dale <jdale9930@gmail.com>
2021-02-19 12:03:53 -0600

    about to add fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchPage from "./components/Search"
 
 function App() {
   return (
     <div className="App">
+      <SearchPage></SearchPage>
     </div>
   );
 }
